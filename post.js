da8c6334a895421daa9e61a9ec2b133e02661a60
flipsewtf <mournstera@proton.me>
2026-05-17 15:22:12 +0200

    code comment added

--- post.js
+++ post.js
@@ -1,3 +1,8 @@
+/*!
+ * post.js — lightbox, audio player
+ * @mournstera | mournstera.tumblr.com
+ */
+
 // -------------------- LIGHTBOX --------------------
 
 (function () {
@@ -173,7 +178,7 @@
 
 // -------------------- AUDIO PLAYER --------------------
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('npf:rendered', () => {
     const playSVG =
         "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' class='play'><path d='M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z'/></svg>";
     const pauseSVG =
